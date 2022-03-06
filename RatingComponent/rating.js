f8c3aa3f08d68cb58381e8c4f1bd3d848ec62bf2
Usama Sulaiman <sulaiman.usama@gmail.com>
2022-03-06 15:44:28 +0530

    Adds rating component

---
+++ RatingComponent/rating.js
@@ -0,0 +1,31 @@
+class RatingComponent {
+  constructor() {
+    this.userRating = 0;
+  }
+
+  // listen to clicks and turn selected level of stars active
+  selectRating(number) {
+    document.querySelectorAll('.star').forEach((star, index) => index < number ? star.classList.add('active') : star.classList.remove('active'));
+  }
+
+  // ⭐️ initialise component to be grayscale by default
+  initialiseRating() {
+    const storedRating = JSON.parse(localStorage.getItem('rating') || '{}');
+    if (storedRating === {} || storedRating === 0) {
+      localStorage.setItem('rating', JSON.stringify(this.userRating));
+      return;
+    } else if (typeof storedRating === 'string') {
+      selectRating(parseInt(storedRating, 10));
+    }
+  }
+
+  // set rating on user action
+  setRating(number) {
+    this.userRating = number === this.userRating ? 0 : number;
+    this.selectRating(this.userRating);
+    localStorage.setItem('rating', JSON.stringify(this.userRating));
+  }
+}
+
+const ratingComp = new RatingComponent();
+ratingComp.initialiseRating();
