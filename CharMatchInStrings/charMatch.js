21ae41cafd988bb0814c84ce0d0e6f3a3ced4452
Usama Sulaiman <sulaiman.usama@gmail.com>
2022-03-06 20:50:24 +0530

    Adds char matching problem

---
+++ CharMatchInStrings/charMatch.js
@@ -0,0 +1,17 @@
+// Given two strings, 'aabbss', 'jjjdww'
+// Return true if any character matches between the two string, otherwise return false
+
+function stringsCompare(string1, string2) {
+  const string1Set = new Set;
+  for (const char in string1) {
+    string1Set.add(char);
+  }
+  for (const char in string2) {
+    if (string1Set.has(char)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log('result is: ', stringsCompare('asdfceddd', 'kkjjiimlo'));
